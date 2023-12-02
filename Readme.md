662ea799cd1bc1ba60e4ee869909a41d30d95db5
Daniel Adeboye <oluwadaunsid@gmail.com>
2023-12-02 11:13:30 +0100

    updated course content

---
+++ Readme.md
@@ -0,0 +1,26 @@
+Introduction
+Installing Python
+Your First Python Program
+How Python Code Gets Executed 
+How Long It Takes To Learn Python 
+Variables
+Receiving Input
+Python Cheat Sheet
+Type Conversion
+Strings
+Formatted Strings
+String Methods
+Arithmetic Operations
+Operator Precedence
+Math Functions
+If Statements
+Logical Operators
+Comparison Operators
+Weight Converter Program 
+While Loops
+Building a Guessing Game
+Building the Car Game
+For Loops
+Nested Loops
+Lists
+2D Lists
---
+++ python Basics/hello.py
@@ -0,0 +1,107 @@
+
+
+
+#  - Basics
+#  - Syntax
+#  - Concepts
+
+print("Hello world this is fredfort consulting")
+
+# Python Beginner's Course Template
+
+# This template introduces basic concepts in Python programming.
+
+# ------------------------------
+# 1. Variables and Data Types
+# ------------------------------
+# Variables are used to store data in a program.
+
+# String: A sequence of characters
+greeting = "Hello, Python Beginner!"
+print(greeting)
+
+# Integer: A whole number
+age = 25
+print(age)
+
+# Float: A decimal number
+height = 5.8
+print(height)
+
+# Boolean: True or False
+is_student = True
+print(is_student)
+
+# ------------------------------
+# 2. Basic Operations
+# ------------------------------
+# Python supports various arithmetic operations.
+
+# Addition
+sum = 5 + 3
+print("Sum:", sum)
+
+# Subtraction
+difference = 10 - 2
+print("Difference:", difference)
+
+# Multiplication
+product = 4 * 2
+print("Product:", product)
+
+# Division
+quotient = 20 / 5
+print("Quotient:", quotient)
+
+# ------------------------------
+# 3. Control Structures
+# ------------------------------
+# Python uses control structures to make decisions and loop through data.
+
+# If statement
+if age > 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+# For loop
+for i in range(5):
+    print("Loop iteration:", i)
+
+# While loop
+count = 0
+while count < 3:
+    print("Count:", count)
+    count += 1
+
+# ------------------------------
+# 4. Functions
+# ------------------------------
+# Functions are blocks of code that perform a specific task.
+
+def greet(name):
+    return "Hello, " + name + "!"
+
+print(greet("Alice"))
+
+# ------------------------------
+# 5. Input and Output
+# ------------------------------
+# Python can take user input and display output.
+
+# Taking user input
+user_name = input("Enter your name: ")
+print("Your name is", user_name)
+
+# ------------------------------
+# Conclusion
+# ------------------------------
+# This template covers the basics of Python.
+# As you advance, you'll learn more about data structures, file handling, error handling, and more.
+
+
+
+
+
+if __name__ ==  "__main__":
+    pass
